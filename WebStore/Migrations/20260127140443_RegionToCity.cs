ba147760b10da93205792fda522a31a22e25a325
antonhag <anton.hagstrom@live.com>
2026-01-27 15:42:19 +0100

    Add Region property to City

---
+++ WebStore/Migrations/20260127140443_RegionToCity.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class RegionToCity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Region",
+                schema: "webstore",
+                table: "Cities",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Region",
+                schema: "webstore",
+                table: "Cities");
+        }
+    }
+}
--- WebStore/Models/City.cs
+++ WebStore/Models/City.cs
@@ -4,6 +4,7 @@ public class City
 {
     public int Id { get; set; }
     public string Name { get; set; } = null!;
+    public string? Region { get; set; }
     
     public int CountryId { get; set; }
     public virtual Country Country { get; set; } = null!;
